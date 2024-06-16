6490b804772945faf8bf2d0487bc01e02d2262ff
Ahmed Al-Omairi <alomairi@mail.ru>
2024-06-17 00:38:17 +0300

    add function showStle to switch styles, make list dropable, arrange functions

--- js/newtab.js
+++ js/newtab.js
@@ -61,43 +61,47 @@ $(document).on("click", "#deleteItem", function () {
 
 });
 
-function changeBookmarkSize(catid, sizeSuffix) {
-    let SettingsAdd = {
-        [catid]
-            :
-            { "size": sizeSuffix }
+$(document).on("click", "#showStyle", function () {
+    const IDitemTochangeStyle = this.attributes.bookid.nodeValue;
+    console.log('IDitemTochangeStyle', IDitemTochangeStyle);
+
+    const currentStyle = this.attributes.bookstyle.nodeValue;
+    console.log('currentStyle', currentStyle);
+
+    const cardstyles = ["bigwide", "smallwide", "bigicon", "smallicon"];
+
+    // Find the index of the current style in the array
+    const currentIndex = cardstyles.indexOf(currentStyle);
+
+    if (currentIndex !== -1) {
+        // Calculate the next index in a circular manner
+        const nextIndex = (currentIndex + 1) % cardstyles.length;
+        
+        // Get the next style from the array
+        const nextStyle = cardstyles[nextIndex];
+        
+        // Update the attribute with the next style
+        $(this).attr('bookstyle', nextStyle);
+
+        // Call the function to change element style with the new style
+        changeElementStyle(IDitemTochangeStyle, currentStyle, nextStyle);
     }
-    let newSetting = {};
-    let templateSettings = { "Settings": {} }
+});
 
-    chrome.storage.local.get('Settings').then((items) => {
-        // console.log(items); 
-        for (const item in items['Settings']) {
-            // console.log();
-            const size = items['Settings'][item].size
-            Object.assign(newSetting, { [item]: { "size": size } });
-        }
 
-        Object.assign(newSetting, SettingsAdd);
-        Object.assign(templateSettings['Settings'], newSetting);
-        console.log(templateSettings);
-
-        chrome.storage.local.set(templateSettings).then(() => {
-            console.log(sizeSuffix + " is setted to category id " + catid);
-        });
-    });
-}
 
 $(document).on("click", "#smallwide", function () {
     const catid = this.attributes.catid.nodeValue;;
     console.log(catid);
-    changeBookmarkSize(catid, "smallwide")
+    // changeBookmarkSize(catid, "smallwide")
+    changeListStyle(".cat" + catid, "bigwide", "smallwide")
 
 });
 $(document).on("click", "#bigwide", function () {
     const catid = this.attributes.catid.nodeValue;;
     console.log(catid);
-    changeBookmarkSize(catid, "bigwide")
+    // changeBookmarkSize(catid, "bigwide")
+    changeListStyle(".cat" + catid, "smallwide", "bigwide")
 
 });
 
@@ -105,6 +109,9 @@ $(document).on("click", "#bigwide", function () {
 function makeCard(value, cat, key, settings) {
     const icon = value.EncodedIcon ? value.EncodedIcon : './icons/default.png';
 
+    if (!settings) {
+        settings = value.Style
+    }
     // Create the card element
     const card = document.createElement('div');
     card.classList.add('cardHolder');
@@ -114,6 +121,7 @@ function makeCard(value, cat, key, settings) {
 
     card.innerHTML = `
         <button class="showDelete btn" id="deleteItem" bookid="${key}">X</button>
+        <button class="showStyle btn" id="showStyle" bookid="${key}" bookstyle="${value.Style}">CH</button>
         <a href="${value.URL}" id="card" class="card-${settings}" style="background: linear-gradient(to left, rgb(${value.Color}), black)">
             <div id="top" class="icon-${settings}">
                 <img id="icon" src="${icon}" alt="" title="local">
@@ -170,7 +178,7 @@ function compareCats(cats, items) {
         }
         else {
             $('#categories').append(`
-         <div class="cat" id="`+ cat + `">
+         <div class="cat cat`+ cat + `" id="` + cat + `">
          <button id="smallwide" class="smallwide btn" catid="`+ cat + `">_</button>
          <button id="bigwide" class="bigwide btn" catid="`+ cat + `">+</button>
          <h2 class="catTitle">`+ cats[cat] + `</h2>  
@@ -189,7 +197,7 @@ function compareCats(cats, items) {
                     settings = title[catid]['size']
                     // console.log(settings);
                 } else {
-                    settings = 'bigwide'
+                    settings = null; // to avoid overwrite Style key in item's json data
                 }
                 makeCard(value, cat, key, settings)
             }
@@ -198,20 +206,109 @@ function compareCats(cats, items) {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    // const numberOfLists = Math.ceil(data.length / 3);
-    const numberOfLists = 5;
 
-    for (let i = 1; i <= numberOfLists; i++) {
-        const newList = document.createElement('div');
-        newList.classList.add('list');
-        newList.id = `list${i}`;
-        document.getElementById("bookmarkbar").appendChild(newList); // Append to bookmarkbar
 
+
+document.addEventListener("DOMContentLoaded", () => {
+        chrome.storage.local.get('topListsSettings').then((items) => {
+            for (const item in items['topListsSettings']) {
+                const id = item;
+                const data = items.topListsSettings[item];
+                const listId = data.list;
+                const pos = data.pos;
+
+                // Get the list element by its ID
+                const list = document.getElementById(listId);
+
+                // Get the element of the item that we will append to the list
+                const itemElement = document.getElementById(id);
+
+                // If both list and item elements exist
+                if (list && itemElement) {
+                    // Get all existing items in the list
+                    const existingItems = list.children;
+
+                    // Ensure position is within bounds
+                    const insertIndex = Math.min(pos - 1, existingItems.length);
+
+                    // If the position is out of bounds, append the item to the end of the list
+                    if (insertIndex === existingItems.length) {
+                        list.appendChild(itemElement);
+                    } else {
+                        // Insert the item at the calculated position
+                        list.insertBefore(itemElement, existingItems[insertIndex]);
+                    }
+                }
+            }
+        });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const dropArea = document.getElementById("dropArea"); // Append to dropArea
+
+    // Add event listener for dragover to allow dropping over dropArea
+    dropArea.addEventListener('dragover', function (event) {
+        event.preventDefault();
+    });
+
+    dropArea.addEventListener('drop', function (event) {
+        event.preventDefault();
+        const targetList = event.target.closest('.drop-area');
+        const itemId = event.dataTransfer.getData('text/plain');
+        const item = document.getElementById(itemId);
+
+        if (item) {
+            targetList.appendChild(item);
+            item.classList.remove('dragging');
+        }
+    });
+
+    makeListDropable();
+});
+
+
+
+// Function to update positions of items in the same list after a drop
+function updateItemPositions() {
+    // Get the container element containing all lists
+    const bookmarksBar = document.getElementById('bookmarkbar');
+    // Get all lists with class "list"
+    const lists = bookmarksBar.querySelectorAll('.list');
+
+    lists.forEach(list => {
+        // Get all items in the current list
+        const itemsToUpdate = Array.from(list.children);
+        itemsToUpdate.forEach((item, index) => {
+            const itemId = item.id;
+            newPosition = index + 1; // Update row number
+            console.log(`Position of item ${itemId} updated to`, newPosition, `on list number`, list.id);
+
+            // Update the position of the item in the template object
+            templateTopListsSettings["topListsSettings"][itemId] = {
+                "list": list.id,
+                "pos": newPosition
+            };
+
+            // Save the updated template object to local storage
+            chrome.storage.local.set(templateTopListsSettings).then(() => {
+                console.log("Value is set");
+            });
+        });
+    });
+}
+
+
+
+
+function makeListDropable() {
+    const lists = document.querySelector("#bookmarkbar");
+    $('.list').each(function (list) {
+        // console.log(this);
 
 
         // Add event listener for dragover to allow dropping over newList
-        newList.addEventListener('dragover', function (event) {
+        this.addEventListener('dragover', function (event) {
             event.preventDefault();
 
             const targetList = event.target.closest('.list');
@@ -224,13 +321,13 @@ document.addEventListener('DOMContentLoaded', function () {
             }
 
         });
-        newList.addEventListener("dragleave", function (event) {
+        this.addEventListener("dragleave", function (event) {
             event.preventDefault();
             console.log('dragleave');
         });
 
         // Add event listener for drop to handle dropping over newList
-        newList.addEventListener('drop', function (event) {
+        this.addEventListener('drop', function (event) {
             event.preventDefault();
             const targetList = event.target.closest('.list');
             const itemId = event.dataTransfer.getData('text/plain');
@@ -265,97 +362,99 @@ document.addEventListener('DOMContentLoaded', function () {
 
             }
         });
-    }
 
-});
-
-// Function to update positions of items in the same list after a drop
-function updateItemPositions() {
-    // Get the container element containing all lists
-    const bookmarksBar = document.getElementById('bookmarkbar');
-    // Get all lists with class "list"
-    const lists = bookmarksBar.querySelectorAll('.list');
-
-    lists.forEach(list => {
-        // Get all items in the current list
-        const itemsToUpdate = Array.from(list.children);
-        itemsToUpdate.forEach((item, index) => {
-            const itemId = item.id;
-            newPosition = index + 1; // Update row number
-            console.log(`Position of item ${itemId} updated to`, newPosition, `on list number`, list.id);
-
-            // Update the position of the item in the template object
-            templateTopListsSettings["topListsSettings"][itemId] = {
-                "list": list.id,
-                "pos": newPosition
-            };
-
-            // Save the updated template object to local storage
-            chrome.storage.local.set(templateTopListsSettings).then(() => {
-                console.log("Value is set");
-            });
-        });
     });
 }
 
+function changeBookmarkSize(catid, sizeSuffix) {
+    let SettingsAdd = {
+        [catid]
+            :
+            { "size": sizeSuffix }
+    }
+    let newSetting = {};
+    let templateSettings = { "Settings": {} }
 
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    chrome.storage.local.get('topListsSettings').then((items) => {
-        for (const item in items['topListsSettings']) {
-            const id = item;
-            const data = items.topListsSettings[item];
-            const listId = data.list;
-            const pos = data.pos;
-
-            // Get the list element by its ID
-            const list = document.getElementById(listId);
-
-            // Get the element of the item that we will append to the list
-            const itemElement = document.getElementById(id);
-
-            // If both list and item elements exist
-            if (list && itemElement) {
-                // Get all existing items in the list
-                const existingItems = list.children;
-
-                // Ensure position is within bounds
-                const insertIndex = Math.min(pos - 1, existingItems.length);
-
-                // If the position is out of bounds, append the item to the end of the list
-                if (insertIndex === existingItems.length) {
-                    list.appendChild(itemElement);
-                } else {
-                    // Insert the item at the calculated position
-                    list.insertBefore(itemElement, existingItems[insertIndex]);
-                }
-            }
+    chrome.storage.local.get('Settings').then((items) => {
+        // console.log(items); 
+        for (const item in items['Settings']) {
+            // console.log();
+            const size = items['Settings'][item].size
+            Object.assign(newSetting, { [item]: { "size": size } });
         }
+
+        Object.assign(newSetting, SettingsAdd);
+        Object.assign(templateSettings['Settings'], newSetting);
+        console.log(templateSettings);
+
+        chrome.storage.local.set(templateSettings).then(() => {
+            console.log(sizeSuffix + " is setted to category id " + catid);
+        });
+
     });
+}
 
-});
+  
 
-document.addEventListener("DOMContentLoaded", () => {
-    const dropArea = document.getElementById("dropArea"); // Append to dropArea
+const changeListStyle = (list, fromSuffix, toSuffix) => {
 
-    // Add event listener for dragover to allow dropping over dropArea
-    dropArea.addEventListener('dragover', function (event) {
-        event.preventDefault();
-    });
+    //Select the container element by its class name
+    const container = document.querySelector(list);
+    const cardHolderslist = container.getElementsByClassName('cardHolder' )
+    console.log('cardHolderslist iss:', cardHolderslist);
 
-    dropArea.addEventListener('drop', function (event) {
-        event.preventDefault();
-        const targetList = event.target.closest('.drop-area');
-        const itemId = event.dataTransfer.getData('text/plain');
-        const item = document.getElementById(itemId);
+            for (i = 0; i < cardHolderslist.length; i++) {
+            var childCard = cardHolderslist[i];
+            // console.log("childCard id is", childCard.id);
 
-        if (item) {
-            targetList.appendChild(item);
-            item.classList.remove('dragging');
+            changeElementStyle(childCard.id, fromSuffix, toSuffix)
         }
-    });
+}
 
-});
+const changeElementStyle = (id, fromSuffix, toSuffix) => {
+
+    //Select the container element by its class name
+    const element = document.getElementById(id)
+    console.log('element  is:', element);
+
+    if (element) {
+
+        //get and change item's style in storage.local
+        chrome.storage.local.get(id).then((item) => {
+            // console.log(item);
+            const currentStyle = item[id]['Style']
+            console.log('currentStyle is', currentStyle);
 
 
+        //  Construct the query selector string dynamically
+        const selector = `.card-${currentStyle}, .icon-${currentStyle}, details-${currentStyle}, .title-${currentStyle}, .domainurl-${currentStyle} , .details-${currentStyle}`;
+
+        // Select all child elements with class names ending in -bigwide
+        const elementsToChange = element.querySelectorAll(selector);
+
+        // Loop through each element and change its class name
+        elementsToChange.forEach(element => {
+            // Get the current class name
+            let currentClassName = element.className;
+
+            // Replace -bigwide with -smallwide in the class name
+            let newClassName = currentClassName.replace(('-' + fromSuffix), '-' + toSuffix);
+
+            // Update the element's class name
+            element.className = newClassName;
+        }) 
+
+        
+
+            item[id]['Style'] = toSuffix
+
+            // console.log(item);
+            chrome.storage.local.set(item).then(() => {
+                console.log(id, " style is changed  ", item[id]['Style'] );
+            });
+
+        });
+    }
+
+
+}
